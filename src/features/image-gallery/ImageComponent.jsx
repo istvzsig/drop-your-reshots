fff20ab2e34e7a8bb0011a656c948d6626ef377a
istvzsig <istvanzsigmond@aol.com>
2024-07-03 00:00:38 +0200

    refactor image gallery

--- src/features/image-gallery/ImageComponent.jsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line react/prop-types
-export default function ImageComponent({ image }) {
-  return (
-    <div className="gallery-item">
-      <img src={image} alt={image}></img>
-    </div>
-  );
-}
